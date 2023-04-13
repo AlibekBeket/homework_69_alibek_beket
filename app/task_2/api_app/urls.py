7fde63018101e7ae5f83abb3f37daa016e617db2
Alibek Beket <alibek1beket@gmail.com>
2023-04-14 03:27:58 +0600

    скопировал все из папки task_1 и перенес в новую папку в task_2 для дз 2, создал приложение 'webapp' и туде добавил файл 'urls.py', добавил новое приложение в settings.py и в urls.py

---
+++ app/task_2/api_app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from api_app.views import json_add_number, get_token_view, json_subtract_number, json_multiply_number, \
+    json_divide_number
+
+urlpatterns = [
+    path('add/', json_add_number, name='add'),
+    path('token/', get_token_view, name='token'),
+    path('subtract/', json_subtract_number, name='subtract'),
+    path('multiply/', json_multiply_number, name='multiply'),
+    path('divide/', json_divide_number, name='divide'),
+]
---
+++ app/task_2/api_app/views.py
@@ -0,0 +1,92 @@
+import json
+
+from django.http import JsonResponse, HttpResponse, HttpResponseNotAllowed
+from django.views.decorators.csrf import ensure_csrf_cookie
+
+
+@ensure_csrf_cookie
+def get_token_view(request, *args, **kwargs):
+    if request.method == 'GET':
+        return HttpResponse()
+    return HttpResponseNotAllowed(f'Only GET request are allowed {request.method}')
+
+
+def json_add_number(request, *args, **kwargs):
+    response_data = {'detail': 'Нет входных данных'}
+    response = JsonResponse(response_data)
+    response.status_code = 200
+    if request.body:
+        number = json.loads(request.body)
+        try:
+            a = int(number.get('A'))
+            b = int(number.get('B'))
+            answer = {'answer': a + b}
+            response = JsonResponse(answer)
+            response.status_code = 201
+        except Exception:
+            response_data = {'error': 'Некорректный набор данных'}
+            response = JsonResponse(response_data)
+            response.status_code = 400
+    return response
+
+
+def json_subtract_number(request, *args, **kwargs):
+    response_data = {'detail': 'Нет входных данных'}
+    response = JsonResponse(response_data)
+    response.status_code = 200
+    if request.body:
+        number = json.loads(request.body)
+        try:
+            a = int(number.get('A'))
+            b = int(number.get('B'))
+            answer = {'answer': a - b}
+            response = JsonResponse(answer)
+            response.status_code = 201
+        except Exception:
+            response_data = {'error': 'Некорректный набор данных'}
+            response = JsonResponse(response_data)
+            response.status_code = 400
+    return response
+
+
+def json_multiply_number(request, *args, **kwargs):
+    response_data = {'detail': 'Нет входных данных'}
+    response = JsonResponse(response_data)
+    response.status_code = 200
+    if request.body:
+        number = json.loads(request.body)
+        try:
+            a = int(number.get('A'))
+            b = int(number.get('B'))
+            answer = {'answer': a * b}
+            response = JsonResponse(answer)
+            response.status_code = 201
+        except Exception:
+            response_data = {'error': 'Некорректный набор данных'}
+            response = JsonResponse(response_data)
+            response.status_code = 400
+    return response
+
+
+def json_divide_number(request, *args, **kwargs):
+    response_data = {'detail': 'Нет входных данных'}
+    response = JsonResponse(response_data)
+    response.status_code = 200
+    if request.body:
+        number = json.loads(request.body)
+        try:
+            a = int(number.get('A'))
+            b = int(number.get('B'))
+            if b == 0:
+                response_data = {'error': 'На 0 делить нельзя'}
+                response = JsonResponse(response_data)
+                response.status_code = 400
+            else:
+                answer = {'answer': a / b}
+                response = JsonResponse(answer)
+                response.status_code = 201
+        except Exception:
+            response_data = {'error': 'Некорректный набор данных'}
+            response = JsonResponse(response_data)
+            response.status_code = 400
+    return response
