7ed8e3d65f5908adf84cd6b0afe5d95b00a86600
Alibek Beket <alibek1beket@gmail.com>
2023-04-14 02:51:44 +0600

    добавил csrftoken и функцию сложения чисел

--- app/api_app/urls.py
+++ app/api_app/urls.py
@@ -1,3 +1,8 @@
-urlpatterns = [
+from django.urls import path
 
+from api_app.views import json_add_number, get_token_view
+
+urlpatterns = [
+    path('add/', json_add_number, name='add'),
+    path('token/', get_token_view, name='token'),
 ]
--- app/api_app/views.py
+++ app/api_app/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http import JsonResponse, HttpResponse, HttpResponseNotAllowed
+from django.views.decorators.csrf import ensure_csrf_cookie
+
+
+@ensure_csrf_cookie
+def get_token_view(request, *args, **kwargs):
+    if request.method == 'GET':
+        return HttpResponse()
+    return HttpResponseNotAllowed(f'Only GET request are allowed {request.method}')
+
+
+def json_add_number(request, *args, **kwargs):
+    if request.method == 'GET':
+        response_data = {'detail': 'Нет входных данных'}
+        response = JsonResponse(response_data)
+        response.status_code = 400
+    if request.body:
+        number = json.loads(request.body)
+        try:
+            a = int(number.get('A'))
+            b = int(number.get('B'))
+            answer = {'answer': a + b}
+            response = JsonResponse(answer)
+            response.status_code = 201
+        except Exception:
+            response_data = {'detail': 'Некорректный набор данных'}
+            response = JsonResponse(response_data)
+            response.status_code = 400
+    return response
